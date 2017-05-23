62e1fc3de1f7220f516c41e59ce73c0585e55f69
Don Hansen <donhansen347@gmail.com>
2017-05-22 21:56:02 -0500

    imported react/redux template, set up basic html for prelaunch design

--- browser/src/react-redux/containers/AppContainer.js
+++ browser/src/react-redux/containers/AppContainer.js
@@ -3,7 +3,47 @@ import React from 'react';
 const AppContainer = (props) => {
   return (
     <div>
-      Hello!
+      <div>
+        <p>Some random text</p>
+        <form>
+          <input type='email' />
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+      <div>
+        <ul>
+          <li>
+            {/* img, text */}
+          </li>
+          <li>
+            {/* img, text */}
+          </li>
+          <li>
+            {/* img, text */}
+          </li>
+          <li>
+            {/* img, text */}
+          </li>
+          <li>
+            {/* img, text */}
+          </li>
+          <li>
+            {/* img, text */}
+          </li>
+        </ul>
+      </div>
+      <div>
+        <p>Hello!</p>
+        <form>
+          <input type='email' />
+          <input type='submit' value='Submit' />
+        </form>
+        <ul>
+          {/*<li><a href='' target='_blank'><icon></li>
+          <li><a href='' target='_blank'><icon></li>
+          <li><a href='' target='_blank'><icon></li>*/}
+        </ul>
+      </div>
     </div>
   );
 };
