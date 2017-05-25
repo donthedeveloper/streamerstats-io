a1ceb358a70edd99235511f760ea0899597523ed
Don Hansen <donhansen347@gmail.com>
2017-05-24 20:19:24 -0500

    current onstream progress of streamerstats prelaunch landing page

--- browser/src/react-redux/containers/AppContainer.js
+++ browser/src/react-redux/containers/AppContainer.js
@@ -3,22 +3,31 @@ import React from 'react';
 const AppContainer = (props) => {
   return (
     <div>
-      <div>
-        <p>Some random text</p>
-        <form>
-          <input type='email' />
-          <input type='submit' value='Submit' />
-        </form>
+      <div className='intro'>
+        <h1 className='title'>Streamer<span>Stats</span></h1>
+        <div className='signup'>
+          <p>This app is really cool. You should totally sign up for it.</p>
+          <form>
+            <input className='input-email' type='email' placeholder='Email Address' />
+            <input className='input-submit' type='submit' value='Submit' />
+          </form>
+        </div>
       </div>
-      <div>
+      <div className='features'>
         <ul>
           <li>
+            <i className="fa fa-bar-chart" aria-hidden="true"></i>
+            <p>Statistics with Google Charts</p>
             {/* img, text */}
           </li>
           <li>
+            <i className="fa fa-bar-chart" aria-hidden="true"></i>
+            <p>Moderation tool to help keep track of things</p>
             {/* img, text */}
           </li>
           <li>
+            <i className="fa fa-bar-chart" aria-hidden="true"></i>
+            <p>Add your feature</p>
             {/* img, text */}
           </li>
           <li>
@@ -32,12 +41,14 @@ const AppContainer = (props) => {
           </li>
         </ul>
       </div>
-      <div>
-        <p>Hello!</p>
-        <form>
-          <input type='email' />
-          <input type='submit' value='Submit' />
-        </form>
+      <div className='gotya'>
+        <div>
+          <p>Hello!</p>
+          <form>
+            <input type='email' />
+            <input type='submit' value='Submit' />
+          </form>
+        </div>
         <ul>
           {/*<li><a href='' target='_blank'><icon></li>
           <li><a href='' target='_blank'><icon></li>
