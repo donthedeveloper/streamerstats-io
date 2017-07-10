8d8190ab564a946078b3d2e73c3501136aa78117
Don Hansen <donhansen347@gmail.com>
2017-07-10 18:25:32 -0500

    express router catchall now servers up index after all other routes have been exhausted, currently api routes before

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../templates/index.html'));
+});
 
 module.exports = router;
