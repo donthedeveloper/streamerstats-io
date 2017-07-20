3301e2b3adb1e4568956a38cb64d43da96ba485d
Don Hansen <donhansen347@gmail.com>
2017-07-20 08:04:35 -0500

    intalled and set up tmi.js

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
+// MODULES
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-
-const router = require('./server/routes');
-
 const chalk = require('chalk');
 
-// middleware
-app.use(morgan('dev'));
+const tmi = require("tmi.js");
+const options = {
+    options: {
+        debug: true
+    },
+    connection: {
+        reconnect: true
+    },
+    identity: {
+        username: "StreamerStatsBot",
+        password: process.env.OAUTH
+    },
+    channels: ["#Gosu"]
+};
+const client = new tmi.client(options);
+client.connect();
 
+
+// MIDDLEWARE
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+// ROUTES
+const router = require('./server/routes');
 
 app.use('/public', express.static('browser/public'));
 
